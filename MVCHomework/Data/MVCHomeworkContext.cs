4ed7af7e1c6e9758cdd56f9f8bec310bb91204d0
MirheyderA <a.mirheyder004@gmail.com>
2024-08-08 23:24:23 +0400

    Add project files.

---
+++ MVCHomework/Data/MVCHomeworkContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCHomework.Models;
+
+namespace MVCHomework.Data
+{
+    public class MVCHomeworkContext : DbContext
+    {
+        public MVCHomeworkContext (DbContextOptions<MVCHomeworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCHomework.Models.Movie> Movie { get; set; } = default!;
+    }
+}
